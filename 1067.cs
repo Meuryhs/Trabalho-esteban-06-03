ac6b7d41d7e679e8b10abe4e6a2374865a636abc
Meuryhs <bxminhauryh@gmail.com>
2025-03-05 19:22:58 -0300

    Add files via upload

---
+++ 1067.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrabalhoEsteban.Exercícios
+{
+    public class Exercicio1067 : IExercicio
+    {
+        public void Executar()
+        {
+            int X = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= X; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Tecle enter para fechar ...");
+            Console.ReadLine();
+        }
+    }
+}
